e2adf08994b35b312b6dcf7bc55ee5ce2e24ffde
Mark Fasel <markfasel@gmail.com>
2024-11-25 09:00:45 -0500

    output function instead of output decorator

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 
@@ -15,7 +15,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   // alternate to decorator approach
   // avatar = input.required<string>();
