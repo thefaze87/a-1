917d30586cead80e8febb58abc6badbc052ddf23
Mark Fasel <markfasel@gmail.com>
2024-11-25 09:12:46 -0500

    using the decorator and set the type for added protection

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -15,8 +15,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
 
   // alternate to decorator approach
   // avatar = input.required<string>();
