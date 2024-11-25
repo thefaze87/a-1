12cf2deb9bd8b43bff43ba821f66efb8fd7efbe8
Mark Fasel <markfasel@gmail.com>
2024-11-25 14:20:11 -0500

    set up with dummy tasks

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
+import { DUMMY_TASKS } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -9,5 +10,11 @@ import { TaskComponent } from '../task/task.component';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required:true }) userId!: string;
+  @Input({ required:true }) name!: string;
+  tasks = DUMMY_TASKS;
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
